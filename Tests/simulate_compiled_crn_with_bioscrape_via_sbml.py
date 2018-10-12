47a91b9bafcdc181c7f8194d542c605cb7d9b88d
William Poole <wpoole@caltech.edu>
2018-10-11 19:17:33 -0700

    Added basic dCas9 functionality, with an example in the Tests folder. Added Global Mechanisms such as degredation from dilution with an example in the Tests folder Cleaned up Parameter inheritance to make it simpler and give (slightly) fewer warnings

---
+++ Tests/simulate_compiled_crn_with_bioscrape_via_sbml.py
@@ -0,0 +1,61 @@
+import bioscrape
+import numpy as np
+import pylab as plt
+import txtl
+import dna_assembly
+
+kb, ku, ktx, ktl, kdeg = 100, 10, 3, 2, 1
+parameters = {"kb":kb, "ku":ku, "ktx":ktx, "ktl":ktl, "kdeg": kdeg}
+myMixture = txtl.BasicExtract(name = "txtl", parameters = parameters)
+
+A1 = dna_assembly.DNAassembly(name = "G1", promoter = "P",
+                              rbs = "BCD", transcript = "T1", protein = "Reporter")
+
+myMixture.add_components(A1)
+CRN = myMixture.compile_crn()
+print("\n"+repr(CRN))
+
+file_name = "bioscrape_test.sbml"
+f = CRN.write_sbml_file(file_name)
+
+#Initial Condition Dict: repr(specie) --> concentration. Default is 0
+#Note in SBML all the species names have ":" replaced with "_".
+initial_condition_dict = {repr(A1.dna):2, "complex_Ribo":10, "protein_RNAP":5, "protein_RNAase":2.5}
+
+#Bioscrape Simulation
+timepoints = np.arange(0, 50, .01)
+print("Simulating")
+sim_result, model= CRN.simulate_with_bioscrape_deterministic(timepoints, f, initial_condition_dict)
+s_l_bs = model.get_species_list()
+
+print([(s, model.get_species_index(s)) for s in s_l_bs])
+
+print([(CRN.species[i], i) for i in range(len(CRN.species))])
+
+result = sim_result.py_get_result()
+rep_ind = model.get_species_index("protein_Reporter")
+rna_rep_ind = model.get_species_index("rna_T1")
+rna_rep_ribo_ind = model.get_species_index("complex_rna_T1_complex_Ribo")
+rna_rep_ase_ind = model.get_species_index("complex_rna_T1_protein_RNAase")
+complex_ribo_ind = model.get_species_index("complex_Ribo")
+
+print("Plotting")
+plt.figure()
+plt.subplot(121)
+plt.plot(timepoints, result[:, rna_rep_ind], label = "rna_Reporter")
+plt.plot(timepoints, result[:, rna_rep_ribo_ind], label = "complex_rna_T1:complex_Ribo")
+plt.plot(timepoints, result[:, rna_rep_ase_ind], label = "complex_rna_T1:protein_RNAase")
+plt.plot(timepoints, result[:, complex_ribo_ind], label = "complex_Ribo")
+
+
+print("result[-1, rna_rep_ind]", result[-1, rna_rep_ind])
+print("result[-1, rna_rep_ribo_ind]", result[-1, rna_rep_ribo_ind])
+print("result[-1, rna_rep_ase_ind]", result[-1, rna_rep_ase_ind])
+print("result[-1, complex_ribo_ind]", result[-1, complex_ribo_ind])
+
+plt.legend()
+
+plt.subplot(122)
+plt.plot(timepoints, result[:, rep_ind], label = "Reporter")
+plt.legend()
+plt.show()
---
+++ d_cas9.py
@@ -0,0 +1,31 @@
+import component
+import mechanism
+import chemical_reaction_network as crn
+
+class guideRNA(component.RNA):
+    def __init__(self, guide_name, dCas9 = "dCas9", **keywords):
+        self.gRNA = crn.specie(guide_name, type = "rna")
+
+        if isinstance(dCas9, crn.specie):
+            self.dCas = dCas9
+        elif isinstance(dCas9, str):
+            self.dCas = crn.specie(dCas9, type = "protein")
+        else:
+            raise ValueError("dCas9 parameter must be a chemical_reaction_network.specie or a string")
+
+
+        self.default_mechanisms = {
+            "dCas9_binding" : mechanism.Reversible_Bimolecular_Binding()
+        }
+        component.RNA.__init__(self, name = guide_name, **keywords)
+
+    def update_species(self):
+        species = [self.gRNA, self.dCas]
+        species += self.mechanisms['dCas9_binding'].update_species(self.gRNA, self.dCas)
+        return species
+
+    def update_reactions(self):
+        ku = self.get_parameter("ku", part_id=self.gRNA.name, mechanism=self.mechanisms['dCas9_binding'])
+        kb = self.get_parameter("kb", part_id=self.gRNA.name, mechanism=self.mechanisms['dCas9_binding'])
+        rxns = self.mechanisms['dCas9_binding'].update_reactions(self.gRNA, self.dCas, kb = kb, ku = ku)
+        return rxns
