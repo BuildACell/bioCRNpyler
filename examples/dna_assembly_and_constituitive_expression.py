34081462bcf3645c3f34da78e5173016d6a0b3ee
zoltuz <zoltuz@gmail.com>
2018-12-05 21:46:11 +0000

    Initial concentrations are added to all species and zero initial concentration is written to the sbml file, if the corresponding species' initial concentration was not set. A simple example and Unit test is added.

--- examples/dna_assembly_and_constituitive_expression.py
+++ examples/dna_assembly_and_constituitive_expression.py
@@ -1,20 +1,23 @@
-from biocrnpyler import * 
+from biocrnpyler import *
 
 kb, ku, ktx, ktl, kdeg = 100, 10, 3, 2, 1
-parameters = {"kb":kb, "ku":ku, "ktx":ktx, "ktl":ktl, "kdeg": kdeg}
-myMixture = BasicExtract(name = "txtl", parameters = parameters)
+parameters = {"kb": kb, "ku": ku, "ktx": ktx, "ktl": ktl, "kdeg": kdeg}
+myMixture = BasicExtract(name="txtl", parameters=parameters)
 
-A1 = DNAassembly(name = "G1", promoter = "pBest",
-                              rbs = "BCD2", transcript = "T1", protein = "GFP")
+A1 = DNAassembly(name="G1", promoter="pBest",
+                 rbs="BCD2", transcript="T1", protein="GFP", initial_conc=10)
 
-#Note: Protein and Transcript strings (or chemical_reaction_network.specie objects) are optional parameters
-#DNAassemblies default to using their name for their transcript and protein products.
+# Note: Protein and Transcript strings (or chemical_reaction_network.specie objects) are optional parameters
+# DNAassemblies default to using their name for their transcript and protein products.
 
 myMixture.add_components(A1)
 myCRN = myMixture.compile_crn()
 
-print("\n"+repr(A1))
-print("\n"+repr(myMixture))
-print("\n"+repr(myCRN))
-#print("\nmyMixture.parameters", myMixture.parameters)
-#print("\ndna_assembly.parameters", A1.parameters)
+print("\n" + repr(A1))
+print("\n" + repr(myMixture))
+print("\n" + repr(myCRN))
+# print("\nmyMixture.parameters", myMixture.parameters)
+# print("\ndna_assembly.parameters", A1.parameters)
+
+file_name = "constitutive_expression_test.xml"
+f = myCRN.write_sbml_file(file_name)
