c993906a1f15363a1460b982a540d01558c910cf
ayush9pandey <ayush.9.pandey@gmail.com>
2019-08-12 21:50:18 -0700

    added licensing information, deleted txtl.py obsolete -> replaced with extracts.py

--- biocrnpyler/crnlab.py
+++ biocrnpyler/crnlab.py
@@ -1,3 +1,6 @@
+
+#  Copyright (c) 2019, Build-A-Cell. All rights reserved.
+#  See LICENSE file in the project root directory for details.
 from .extracts import BasicExtract
 from .mixture import Mixture
 from .dna_assembly import DNAassembly
