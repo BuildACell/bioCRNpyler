8233c2cfeb4cc427a19f62b6bf77df3851b57b4a
Andrey Shur <andrey.shur@gmail.com>
2020-05-04 17:53:14 -0700

    thinking about how to explore integrase reactions

--- biocrnpyler/dna_construct.py
+++ biocrnpyler/dna_construct.py
@@ -9,6 +9,8 @@
 from matplotlib import cm
 import matplotlib.pyplot as plt
 import random
+from warnings import warn
+integrase_sites = ["attB","attP","attL","attR","FLP","CRE"]
 class DNA_part:
     def __init__(self,name,biocrnpyler_class=None,attributes={},**keywords):
         """this represents a sequence. These get compiled into working components"""
@@ -22,7 +24,9 @@ class DNA_part:
         self.protein = None
         self.no_stop_codons = []
         self.attributes = attributes
-
+        self.pos = None
+        self.parent_dna = None
+        self.direction=None
 
         if("no_stop_codons" in attributes):
             self.no_stop_codons = attributes["no_stop_codons"]
@@ -31,7 +35,7 @@ class DNA_part:
         if("part_type" in attributes):
             #acceptable part 
             self.part_type = attributes["part_type"]
-        if(self.part_type in ["attB","attP","attL","attR"]):
+        if(self.part_type in integrase_sites):
             if("integrase" in attributes):
                 self.integrase = attributes["integrase"]
             else:
@@ -48,12 +52,39 @@ class DNA_part:
                 self.protein = attributes["protein"]
     def __repr__(self):
         myname = self.name
-        if(self.part_type in ["attB","attP","attL","attR"]):
+        if(self.part_type in integrase_sites):
             myname += "-" + self.integrase
             if(self.dinucleotide != 1):
                 myname += "-"+str(self.dinucleotide) 
+        if(self.direction =="reverse"):
+            myname += "-r"
         return myname
-    def dnaplotlib_design(self,direction=True,color=(1,4,2),color2=(3,2,4)):
+    def clone(self,position,direction,parent_dna):
+        """this defines where the part is in what piece of DNA"""
+        self.pos = position
+        self.direction = direction
+        self.parent_dna = parent_dna
+        return self
+    def unclone(self):
+        """removes the current part from anything"""
+        self.pos = None
+        self.direction = None
+        self.parent_dna = None
+        return self
+    def reverse(self):
+        if(self.direction=="forward"):
+            self.direction = "reverse"
+        elif(self.direction=="reverse"):
+            self.direction = "forward"
+        elif(self.direction==None):
+            warn(self.name+" has no direction. Perhaps it wasn't cloned?")
+        else:
+            raise ValueError("direction is not forward or reverse! It's "+self.direction)
+    def dnaplotlib_design(self,direction=None,color=(1,4,2),color2=(3,2,4)):
+        if(direction==None and self.direction != None):
+            direction == self.direction=="forward"
+        elif(direction==None):
+            direction = True
         dnaplotlib_dict = {\
             "promoter":"Promoter",\
             "rbs":"RBS",\
@@ -71,6 +102,26 @@ class DNA_part:
             outdesign = outdesign[::-1]
         return outdesign
 
+class IntegraseMechanism:
+    def __init__(self,name,reactions={("attB","attP"):("attL","attR")}):
+        self.reactions = reactions
+        self.attsites = []
+        for reactants in reactions:
+            self.attsites+=list(reactants)+list(reactions[reactants])
+    def binds_to(self):
+        return self.attsites
+    def integrate(site1,site2,chassis):
+        """perform an integration reaction between the chosen sites and alter the chassis"""
+        reactants1 = chassis.extract_construct(site1)
+
+        reactants2 = chassis.extract_construct(site2)
+
+        for reactant1 in reactants1:
+
+            for reactant2 in reactants2:
+                pass
+
+        
 
 from copy import deepcopy as dc
 def rev_dir(dir):
@@ -80,12 +131,23 @@ class DNA_construct:
     def __init__(self,parts_list,circular=False,**keywords):
         """this represents a bunch of parts in a row.
         A parts list has [[part,direction],[part,direction],...]"""
-        self.parts_list = parts_list
+        myparts = []
+        curpos = 0
+        for part in parts_list:
+            if(isinstance(part,list)):
+                #if you give it a list, then that means the second element of the
+                #list is the direction
+                myparts += [dc(part[0]).clone(curpos,part[1],self)]
+            elif(isinstance(part,DNA_part)):
+                #in this case the direction wasn't provided, but we assume it's forward!
+                myparts += [dc(part[0]).clone(curpos,"forward",self)]
+            curpos += 1
+        self.parts_list = myparts
         self.circular=circular
     def explore_txtl(self):
         """this function finds promoters and terminators and stuff in the construct"""
         made_proteins = {}
-        all_rnas = []
+        all_rnas = {}
         current_rnas = {}
         current_proteins = {}
         current_rbs = None
@@ -121,12 +183,12 @@ class DNA_construct:
                 if(self.circular == False):
                     previous_part = None
                 #'''
-                effective_direction = part[1]
+                effective_direction = part.direction
 
                 if(direction=="reverse"):
                     #if we are reverse then everything is backwards
                     effective_direction = rev_dir(effective_direction)
-                part_object = part[0]
+                part_object = part
 
 
                 if(current_rnas!={}):
@@ -171,7 +233,9 @@ class DNA_construct:
                         if(part_object.part_type=="terminator" and effective_direction == "forward"):
                             terminated = 1 #a terminator ends everything. This does not account for leakiness
                             #this stops the RNA! but only in the forward direction
-                            current_rna_name = str(promoter)+"="+'_'.join([str(a[0])+"-r"*(a[1]=="reverse") for a in current_rnas[promoter]])
+                            rna_partslist = current_rnas[promoter]
+                            rna_construct = DNA_construct(rna_partslist,circular=False)
+                            current_rna_name = str(promoter)+"="+str(rna_construct)
                             made_proteins[current_rna_name]={}
                             #compile the name, keeping track of which promoter made the rna and all the parts on it
                             for rbs in current_proteins:
@@ -187,7 +251,7 @@ class DNA_construct:
                                 made_proteins[current_rna_name].update({rbs:proteins_per_rbs})
                                 #we want to clear the current proteins, but there could be multiple RNAs that run over the same proteins,
                                 #so wait until we are done tallying all the RNAs before removing everything from current_proteins
-                            all_rnas +=[[promoter,current_rnas[promoter]]]
+                            all_rnas.update({promoter:current_rnas[promoter]})
                     
                         
                     if(terminated):
@@ -199,30 +263,97 @@ class DNA_construct:
                     #this part is a promoter, so it transcribes!
                     current_rnas.update({part_object:[]})
         return all_rnas,made_proteins
-    '''
-    def explore_integrase(self):
-        """this function finds all possible DNA states that can be attained by reactions
-        between compatible integrase sites"""
-        #TODO this function should be global for all constructs!!
-        integrase_mechanism = {"attB":"attP","attP":"attB"}
-        integrases_react = {}
-        for part in self.parts_list:
-            if(part.integrase!= None):
-
-                integrases_react[part.integrase] = integrases_react[part.integrase]+part
-    #'''
     def __repr__(self):
         output = ""
-        output = '_'.join([str(a[0])+"-r"*(a[1]=="reverse") for a in self.parts_list])
+        output = '_'.join([str(a) for a in self.parts_list])
         if(self.circular):
             output+="-o"
         return output
+    def list_integrase(self,int_dict={}):
+        """lists all the parts that can be acted on by integrases"""
+        for part in self.parts_list:
+            if(part.integrase != None):
+                if(part.integrase in int_dict):
+                    int_dict.update({part.integrase:int_dict[part.integrase]+[part]})
+                else:
+                    int_dict[part.integrase]=[part]
+        return int_dict
+                    
+    def __contains__(self,obj2):
+        """we should be able to check if this construct has a certain part"""
+        if(isinstance(obj2,DNA_part)):
+            #if we got a DNA part it could mean one of two things:
+            #1 we want to know if a dna part is anywhere
+            #2 we want to know if a specific DNA part is in here
+            #this is complicated by the fact that we want to have the same DNA part be reusable
+            #in many locations
+            if(obj2.parent_dna==self):
+                #the object should already know if it's a part of me
+                return True
+            elif(obj2.parent_dna==None):
+                #this object has been orphaned. 
+                #that means we are looking for matching objects in any position
+                new_obj2 = dc(obj2).unclone()
+                uncloned_list = [a.unclone() for a in dc(self.parts_list)]
+                return new_obj2 in uncloned_list
+            else:
+                return False
+        elif(isinstance(obj2,str)):
+            #if we get a string, that means we want to know if the name exists anywhere
+            return obj2 in str(self)
+
     def dnaplotlib_design(self):
         outdesign = []
         cmap = cm.Set1(range(len(self.parts_list)))
         pind = 0
         for part in self.parts_list:
-
-            outdesign+=part[0].dnaplotlib_design(part[1]=="forward",color=cmap[pind][:-1],color2 = random.choice(cmap)[:-1])
+            outdesign+=part.dnaplotlib_design(part.direction=="forward",color=cmap[pind][:-1],color2 = random.choice(cmap)[:-1])
             pind+=1
         return outdesign
+class Chassis():
+    def __init__(self,dna_constructs,name="myChassis"):
+        """a chassis is a combination of DNA parts. This is either your TXTL tube which can contain
+        multiple different DNA molecules, or your cell, which also contains multiple different
+        DNA molecules. This is useful because interactions between different DNA
+        molecules that are part of the same test tube can occur"""
+        self.dna_constructs = dna_constructs
+        self.name = name
+    def extract_construct(self,part):
+        """extract a DNA_construct which contains part.
+        part can be a DNA_part or a string which matches part of
+        the DNA_construct's name"""
+        conlist = []
+        for construct in self.dna_constructs:
+            if(part in construct):
+                conlist+= [construct]
+        return conlist
+
+    def explore_integrases(self,intnames=None):
+        """this explores all the possible integrase-motivated DNA configurations. If some
+        integrases aren't present, then define intnames to be a list of names of the
+        integrases which are present.
+
+        An integrase can act in different ways. 
+        * serine integrases recombine B and P sites that turn into L and R sites, 
+                    and only sites with the same dinucleotide can be recombined.
+        * serine integrases with directionality factors recombine L and R sites
+                    with the same dinucleotide
+        * Invertases only do flipping reactions
+        * resolvases only do deletion reactions
+        * FLP or CRE react with homotypic sites, so site1+site1 = site1+site1. But
+                    there are still different types of sites which are orthogonal. For
+                    example, a CRE type 1 or a CRE type 2 site. The sites can also be palindromic,
+                    which means that they can react in either direction.
+        """
+        int_dict = {}
+        for construct in self.dna_constructs:
+            #list each integrase that exists and which sites they react with
+            int_dict = construct.list_integrase(int_dict)
+        for integrase in int_dict:
+            #now, going through each one, generate the reactions and species that arise
+            if(integrase in intnames or intnames==None):
+                #this only reacts with integrases that we said exist, or all of them if we didn't
+                #say anyhting
+                attsites = int_dict[integrase]
+                #but now we need to know what kind of integrase reactions are possible
+
