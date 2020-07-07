b91bd45feaab9c6cf6a4f077a07fc8225f08a60a
dr3y <andrey.shur@gmail.com>
2020-07-07 15:47:08 -0700

    added "is not None" to a bunch of stuff (#118)
    * try except for pandas, seaborn
    
    * added somemore try blocks
    
    * missed an if statement
    
    * modify plotting text
    
    * added "is not None" to a lot of stuff
    
    * Fixed Simulate with Bioscrape Functions & Tests
    
    The tests now each have two cases: one without bioscrape and one with bioscrape.
    
    Co-authored-by: William Poole <wpoole@caltech.edu>

--- Tests/test_simulation_packages_import.py
+++ Tests/test_simulation_packages_import.py
@@ -5,42 +5,55 @@ import pytest
 
 
 def test_bioscrape_import_simulate():
-    from biocrnpyler import ChemicalReactionNetwork
+    from biocrnpyler import ChemicalReactionNetwork, Species
+    import numpy as np
 
-    CRN = ChemicalReactionNetwork(species=[],reactions=[])
+    X = Species("X")
+    CRN = ChemicalReactionNetwork(species=[X],reactions=[])
     with pytest.warns(None) as record:
-        sim_result = CRN.simulate_with_bioscrape(timepoints=[], initial_condition_dict = {})
+        sim_result = CRN.simulate_with_bioscrape(timepoints=np.linspace(0, 10, 100), initial_condition_dict = {str(X):1})
 
-    assert sim_result is None
 
-    # only one warning was triggered
-    assert len(record) == 1
-    # check the warning message
-    assert str(record[0].message) == "bioscrape was not found, please install bioscrape"
-
+    #In the case bioscrape is not imported
+    if sim_result is None:
+        # only one warning was triggered
+        assert len(record) == 1
+        # check the warning message
+        assert str(record[0].message) == "bioscrape was not found, please install bioscrape"
+    #In the case bioscrape is imported
+    else:
+        assert str(X) in sim_result
 
 def test_bioscrape_import_simulate_via_sbml():
-    from biocrnpyler import ChemicalReactionNetwork
+    from biocrnpyler import ChemicalReactionNetwork, Species
+    import numpy as np
 
-    CRN = ChemicalReactionNetwork(species=[],reactions=[])
+    X = Species("X")
+    CRN = ChemicalReactionNetwork(species=[X],reactions=[])
     with pytest.warns(None) as record:
-        sim_result, bioscrape_model  = CRN.simulate_with_bioscrape_via_sbml(timepoints=[], initial_condition_dict={})
+        sim_result, bioscrape_model  = CRN.simulate_with_bioscrape_via_sbml(timepoints=np.linspace(0, 10, 100), initial_condition_dict={str(X):1})
 
-    assert sim_result is None
-    assert bioscrape_model is None
 
-    # only one warning was triggered
-    assert len(record) == 1
-    # check the warning message
-    assert str(record[0].message) == "bioscrape was not found, please install bioscrape"
+    #In the case bioscrape is not imported
+    if sim_result is None and bioscrape_model is None:
+        # only one warning was triggered
+        assert len(record) == 1
+        # check the warning message
+        assert str(record[0].message) == "bioscrape was not found, please install bioscrape"
+
+    #In the case bioscrape is imported
+    else:
+        assert str(X) in sim_result
+        assert bioscrape_model is not None
 
 
 def test_libroadrunner_import():
     from biocrnpyler import ChemicalReactionNetwork
+    import numpy as np
 
     CRN = ChemicalReactionNetwork(species=[],reactions=[])
     with pytest.warns(None) as record:
-        sim_results = CRN.runsim_roadrunner(timepoints=[], filename=None)
+        sim_results = CRN.runsim_roadrunner(timepoints=np.linspace(0, 10, 100), filename=None)
 
     assert sim_results is None
 
