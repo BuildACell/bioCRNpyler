b650d07fe9b7bbb68fbd7794d85564339ab3ab86
zoltuz <zoltuz@gmail.com>
2020-07-24 22:17:20 +0100

    add propensity unittest for non-massaction types

--- Tests/test_propensities.py
+++ Tests/test_propensities.py
@@ -1,9 +1,11 @@
 #  Copyright (c) 2020, Build-A-Cell. All rights reserved.
 #  See LICENSE file in the project root directory for details.
 
-from biocrnpyler import MassAction
+from biocrnpyler import MassAction, ProportionalHillNegative, ProportionalHillPositive, HillPositive, HillNegative
 from biocrnpyler import ParameterEntry
+from biocrnpyler import Species
 import pytest
+from biocrnpyler import sbmlutil
 
 
 def test_massaction_forward_rate():
@@ -30,13 +32,129 @@ def test_massaction_is_reverable():
     mak = MassAction(k_forward=1, k_reverse=0.1)
     assert mak.is_reversible
 
+
 def test_massaction_from_parameters():
-    mak = MassAction(k_forward = ParameterEntry("k", .1))
-    assert(mak.k_forward == .1)
+    mak = MassAction(k_forward=ParameterEntry("k", .1))
+    assert mak.k_forward == .1
 
-    mak = MassAction(k_forward = ParameterEntry("k", .1), k_reverse = ParameterEntry("k", .01))
-    assert(mak.k_reverse == .01)
+    mak = MassAction(k_forward=ParameterEntry("k", .1), k_reverse=ParameterEntry("k", .01))
+    assert mak.k_reverse == .01
+
+
+def test_hill_negative_init():
+    s1 = Species('s1')
+    hillneg = HillNegative(k=1, s1=s1, K=5, n=2)
+    assert s1 == hillneg.s1
+
+    assert hillneg.is_reversible is False
+
+    with pytest.raises(TypeError, match='Propensity expected a Species'):
+        HillNegative(k=1, s1='s1', K=5, n=2)
+
+    with pytest.raises(TypeError):
+        d = Species('d')
+        HillNegative(k=1, s1=s1, K=5, n=2, d=d)
+
+
+def test_hill_negative_rate_formula():
+    in_s1 = Species('s1')
+    in_k = 1
+    in_K = 5
+    in_n = 2
+    philpos = HillNegative(k=in_k, s1=in_s1, K=in_K, n=in_n)
+
+    assert philpos._get_rate_formula(philpos.propensity_dict) == f"{in_k}/({in_s1}^{in_n}+{in_K})"
+
+
+def test_hill_positive_init():
+    s1 = Species('s1')
+    philpos = HillPositive(k=1, s1=s1, K=5, n=2)
+    assert s1 == philpos.s1
+
+    assert philpos.is_reversible is False
+
+    with pytest.raises(TypeError, match='Propensity expected a Species'):
+        HillPositive(k=1, s1='s1', K=5, n=2)
+
+    with pytest.raises(TypeError):
+        d = Species('d')
+        HillPositive(k=1, s1=s1, K=5, n=2, d=d)
+
+
+def test_hill_positive_rate_formula():
+    in_s1 = Species('s1')
+    in_k = 1
+    in_K = 5
+    in_n = 2
+    philpos = HillPositive(k=in_k, s1=in_s1, K=in_K, n=in_n)
+
+    assert philpos._get_rate_formula(philpos.propensity_dict) == f"{in_k}*{in_s1}^{in_n}/({in_s1}^{in_n}+{in_K})"
+
+
+def test_proportional_hill_positive_init():
+    d = Species('d')
+    s1 = Species('s1')
+    philpos = ProportionalHillPositive(k=1, s1=s1, K=5, n=2, d=d)
+    assert d == philpos.d
+    assert s1 == philpos.s1
+
+    assert philpos.is_reversible is False
+
+    with pytest.raises(TypeError, match='Propensity expected a Species'):
+        ProportionalHillPositive(k=1, s1=s1, K=5, n=2, d='')
+
+    with pytest.raises(TypeError, match='Propensity expected a Species'):
+        ProportionalHillPositive(k=1, s1='s1', K=5, n=2, d=d)
+
+
+def test_proportional_hill_positive_rate_formula():
+    in_d = Species('d')
+    in_s1 = Species('s1')
+    in_k = 1
+    in_K = 5
+    in_n = 2
+    philpos = ProportionalHillPositive(k=in_k, s1=in_s1, K=in_K, n=in_n, d=in_d)
+
+    assert philpos._get_rate_formula(philpos.propensity_dict) == f"{in_k}*{in_d}*{in_s1}^{in_n}/({in_s1}^{in_n}+{in_K})"
+
+
+def test_proportional_hill_negative_init():
+    d = Species('d')
+    s1 = Species('s1')
+    philneg = ProportionalHillNegative(k=1, s1=s1, K=5, n=2, d=d)
+    assert d == philneg.d
+    assert s1 == philneg.s1
+
+    assert philneg.is_reversible is False
+
+    with pytest.raises(TypeError, match='Propensity expected a Species'):
+        ProportionalHillNegative(k=1, s1=s1, K=5, n=2, d='')
+
+    with pytest.raises(TypeError, match='Propensity expected a Species'):
+        ProportionalHillNegative(k=1, s1='s1', K=5, n=2, d=d)
+
+
+def test_proportional_hill_negative_rate_formula():
+    in_d = Species('d')
+    in_s1 = Species('s1')
+    in_k = 1
+    in_K = 5
+    in_n = 2
+    philneg = ProportionalHillNegative(k=in_k, s1=in_s1, K=in_K, n=in_n, d=in_d)
+
+    assert philneg._get_rate_formula(philneg.propensity_dict) == f"{in_k}*{in_d}/({in_s1}^{in_n}+{in_K})"
 
 
 
+
+    # document, model = sbmlutil.create_sbml_model()
+    #
+    # # Create the sbml_reaction in SBML
+    # sbml_reaction = model.createReaction()
+    # all_ids = sbmlutil.getAllIds(model.getSBMLDocument().getListOfAllElements())
+    # trans = sbmlutil.SetIdFromNames(all_ids)
+    # sbml_reaction.setId(trans.getValidIdForName('r1'))
+    # sbml_reaction.setName(sbml_reaction.getId())
+    #
+    # philneg.create_kinetic_law(model=model, sbml_reaction=sbml_reaction, stochastic=False)
 
