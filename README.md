c7858c8cb4317a6fdaa067d210396d257205a99e
ayush9pandey <ayush.9.pandey@gmail.com>
2020-07-31 01:05:42 -0700

    modify setup.py for PyPi installation

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # BioCRNPyler -- Biomolecular Chemical Reaction Network Compiler
-## Python toolbox to create CRN models in SBML for biomolecular mechanisms 
+## Python toolbox to create CRN models in SBML for biomolecular mechanisms
 
 [![Build Status](https://travis-ci.com/BuildACell/BioCRNPyler.svg?branch=master)](https://travis-ci.com/BuildACell/BioCRNPyler)
 [![codecov](https://codecov.io/gh/BuildACell/BioCRNPyler/branch/master/graph/badge.svg)](https://codecov.io/gh/BuildACell/BioCRNPyler)
--- setup.py
+++ setup.py
@@ -6,33 +6,37 @@ with open('README.md') as fp:
 
 setup(
     name='biocrnpyler',
-    version='1.0.0',
+    version='1.0.1',
     author='BuildACell',
-    url='https://github.com/BuildACell/biocrnplyler/',
+    url='https://github.com/BuildACell/biocrnpyler/',
     description='A chemical reaction network compiler for generating large biological circuit models',
     long_description=long_description,
     packages=['biocrnpyler'],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Science/Research',
-        'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Topic :: Software Development',
         'Topic :: Scientific/Engineering',
-        'Operating System :: POSIX',
-        'Operating System :: Unix'
-        'Operating System :: MacOS'
+        'Operating System :: OS Independent',
     ],
     install_requires=[
           'python-libsbml',
           'numpy',
-          'matplotlib<=3.2.2',
+          'matplotlib',
           'networkx',
-          'bokeh>=1.4.0',
+          'bokeh',
           'fa2',
       ],
     setup_requires=["pytest-runner"],
+    python_requires='>=3.6',
+    keywords="SBML synthetic biology modeling Chemical Reaction Network CRN model",
     tests_require=["pytest", "pytest-cov", "nbval"],
+    project_urls={
+    'Documentation': 'https://readthedocs.org/projects/biocrnpyler/',
+    'Funding': 'http://www.cds.caltech.edu/~murray/wiki/index.php?title=Developing_Standardized_Cell-Free_Platforms_for_Rapid_Prototyping_of_Synthetic_Biology_Circuits_and_Pathways',
+    'Source': 'https://github.com/BuildACell/biocrnpyler',
+    'Tracker': 'https://github.com/BuildACell/BioCRNPyler/issues',
+    },
 )
