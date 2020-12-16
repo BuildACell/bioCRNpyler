4998a1bb15a828ebb602617eba997fe21aaa5ff6
William Poole <wpoole@caltech.edu>
2020-12-16 10:57:50 -0800

    started making tests

--- Tests/test_initial_condition.py
+++ Tests/test_initial_condition.py
@@ -1,11 +1,28 @@
 #  Copyright (c) 2019, Build-A-Cell. All rights reserved.
 #  See LICENSE file in the project root directory for details.
 
+import pytest
 from unittest import TestCase
-from biocrnpyler import Species, Mixture, Component, ParameterDatabase
+from biocrnpyler import Species, Mixture, Component, ParameterDatabase, ChemicalReactionNetwork
+
+class TestOrderedMonomer(TestCase):
+
+    def test_species_initial_condition_in_crn(self):
+        S = Species("S", initial_concentration = 1.1)
+        CRN = ChemicalReactionNetwork(species = [S], reactions = [])
+        self.assertTrue(CRN.species[0].initial_concentration == 1.1)
 
 
-class TestSpecies(TestCase):
-	#TODO - wait for revamp of Species because that will likely change this
-	pass
+
+    def test_species_initial_condition_in_mixture(self):
+        S = Species("S", initial_concentration = 1.0)
+        M = Mixture(species = [S])
+        CRN = M.compile_crn()
+        print("S", CRN.species[0])
+        print("initial concentration", CRN.species[0].initial_concentration)
+        self.assertTrue(CRN.species[0].initial_concentration == 1.1)
+
+
+
+	
 
