94b47dd49c9a78fb1a139a6a72faa735312c6f71
Andrey Shur <andrey.shur@gmail.com>
2021-05-25 16:43:15 -0700

    remove typeshed

--- Tests/test_polymer.py
+++ Tests/test_polymer.py
@@ -1,7 +1,6 @@
 #  Copyright (c) 2019, Build-A-Cell. All rights reserved.
 #  See LICENSE file in the project root directory for details.
 
-from _typeshed import NoneType
 from unittest import TestCase
 from biocrnpyler import OrderedPolymer, OrderedMonomer, OrderedPolymerSpecies, Species, Complex, MonomerCollection
 
