4576bc3ac6086f02835fa201d6b4b283639b7fbf
Zoltan Tuza <zoltuz@gmail.com>
2021-12-19 23:08:20 +0000

    [docs] add missing package to doc gen

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -17,7 +17,7 @@ set -x
 ###################
 
 apt-get update
-apt-get -y install git rsync python3 python3-pip
+apt-get -y install git rsync python3 python3-pip libffi-dev libssl-dev pandoc
 
 #####################
 # DECLARE VARIABLES #
