7c928bd296ecc425962ef110bf5bed544e47e616
Zoila Jurado <32440023+zjuradoq@users.noreply.github.com>
2025-08-13 17:49:35 -0400

    Update membrane.py

--- biocrnpyler/components/membrane.py
+++ biocrnpyler/components/membrane.py
@@ -80,7 +80,7 @@ class IntegralMembraneProtein(Component):
         
         self.membrane_protein = self.set_species(membrane_protein)
 
-        if self.membrane_protein.compartment.name not in ('default', compartment.name):
+        if self.membrane_protein.compartment.name not in ('default', compartment):
             warnings.warn("Inconsistent compartment with IntegralMembraneProtein and membrane_protein. Defaulting to use IntegralMembraneProtein compartment.", UserWarning) #<--can change this error message to be more specific
             self.membrane_protein.compartment.name = compartment.name # uses membrane protein compartment going forward
             # compartment = self.membrane_protein.compartment.name  # uses compartment parameter going forward
